afeb6edaadadab155c6994790a52fde79209884a
hsiaoy71950 <51727091+hsiaoy71950@users.noreply.github.com>
2021-08-02 10:05:10 +1200

    yes haha

--- content/showall.php
+++ content/showall.php
@@ -7,4 +7,23 @@ $find_sql= "SELECT * FROM `quote_table` JOIN author_table ON (`author_table`.`Au
 $find_query = mysqli_query($conn, $find_sql);
 $find_rs = mysqli_fetch_assoc($find_query);
 
+do {
+
+    $quote = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ', $find_rs['Quote']);
+    $author = $find_rs['Author']
+?>
+
+<div class="results">
+    <?php echo $quote; ?><br />
+    <?php echo $author; ?>
+</div>
+
+<br />
+
+<?php
+
+}
+
+while($find_rs = mysqli_fetch_assoc($find_query))
+
 ?>
